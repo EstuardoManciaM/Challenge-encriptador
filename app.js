16e473937488356f913b6c90a5c41bdb15f88ca9
EstuardoMMM <estuardo74972897@gmail.com>
2024-02-07 21:13:44 -0600

    Creación de la lógica de la encriptación

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-function EncriptarTexto(){
-    let textoUsuario= document.getElementById("textoEncriptado").value;
-   return(console.log(textoUsuario));
+function EncriptarTexto() {
+    let letra = 0;
+    let texto = [];
+    let textoSalida = "";
+    let textoUsuario = document.getElementById("textoEncriptado").value;
+
+    for (letra = 0; letra < textoUsuario.length; letra++) {
+        texto.push(textoUsuario.charAt(letra));
+        if (texto[letra] == "a") {
+            texto[letra] = "ai";
+        } else if (texto[letra] == "e") {
+            texto[letra] = "enter";
+        } else if (texto[letra] == "i") {
+            texto[letra] = "imes";
+        } else if (texto[letra] == "o") {
+            texto[letra] = "ober";
+        } else if (texto[letra] == "u") {
+            texto[letra] = "ufat";
+        }
+    }
+    textoSalida += texto;
+    return (console.log(textoSalida.replaceAll(",", "")));
 }
 
 EncriptarTexto();
